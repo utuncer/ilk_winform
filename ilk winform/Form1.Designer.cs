a5b6732e81a12b16131270500ab64afe56c580b8
Umut Tunçer <utuncerx@gmail.com>
2024-11-19 16:15:12 +0300

    Form Elemanlarından Label Anlaşıldı

--- ilk winform/Form1.Designer.cs
+++ ilk winform/Form1.Designer.cs
@@ -28,20 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.firstlabel = new System.Windows.Forms.Label();
+            this.firstbutton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // firstlabel
+            // 
+            this.firstlabel.AutoSize = true;
+            this.firstlabel.Location = new System.Drawing.Point(77, 106);
+            this.firstlabel.Name = "firstlabel";
+            this.firstlabel.Size = new System.Drawing.Size(44, 16);
+            this.firstlabel.TabIndex = 0;
+            this.firstlabel.Text = "label1";
+            // 
+            // firstbutton
+            // 
+            this.firstbutton.Location = new System.Drawing.Point(251, 89);
+            this.firstbutton.Name = "firstbutton";
+            this.firstbutton.Size = new System.Drawing.Size(75, 23);
+            this.firstbutton.TabIndex = 1;
+            this.firstbutton.Text = "button1";
+            this.firstbutton.UseVisualStyleBackColor = true;
+            this.firstbutton.Click += new System.EventHandler(this.firstbutton_Click);
             // 
             // mainform
             // 
             this.ClientSize = new System.Drawing.Size(1093, 416);
+            this.Controls.Add(this.firstbutton);
+            this.Controls.Add(this.firstlabel);
             this.Name = "mainform";
             this.Load += new System.EventHandler(this.mainform_Load);
             this.Click += new System.EventHandler(this.mainform_Click);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Label firstlabel;
         private System.Windows.Forms.Button firstbutton;
     }
 }
--- ilk winform/Form1.cs
+++ ilk winform/Form1.cs
@@ -19,23 +19,33 @@ namespace ilk_winform
 
         private void mainform_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("evete basıldı");
+            // MessageBox.Show("Form Yüklendi");
         }
 
         private void mainform_Click(object sender, EventArgs e)
         {
             #region Basit Etkileşimli Mesaj Kutusu
-            string message = "Bilinmeyen bir hata oluştu.";
-            DialogResult result = MessageBox.Show(message, "Hata !!", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
-            if (result == DialogResult.Yes)
-            {
-                MessageBox.Show("evete basıldı");
-            }
-            if (result == DialogResult.No)
-            {
-                MessageBox.Show("hayıra basıldı");
+            //string message = "Bilinmeyen bir hata oluştu.";
+            //DialogResult result = MessageBox.Show(message, "Hata !!", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+            //if (result == DialogResult.Yes)
+            //{
+            //    MessageBox.Show("evete basıldı");
+            //}
+            //if (result == DialogResult.No)
+            //{
+            //    MessageBox.Show("hayıra basıldı");
 
-            } 
+            //} 
+            #endregion
+
+        }
+
+        private void firstbutton_Click(object sender, EventArgs e)
+        {
+            #region Butona Tıkladığında Yazıyı (Label'ı) Düzenleyen Komut
+            //firstlabel.Text = "deneme";
+            //firstlabel.BackColor = Color.IndianRed;
+            //firstlabel.ForeColor = Color.White;
             #endregion
         }
     }

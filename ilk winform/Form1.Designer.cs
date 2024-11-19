528043b84b044d74eb599ac4b18c530339615cdb
Umut Tunçer <utuncerx@gmail.com>
2024-11-19 16:39:48 +0300

    Form Elemanlarından Panel ve GroupBox Anlaşıldı

--- ilk winform/Form1.Designer.cs
+++ ilk winform/Form1.Designer.cs
@@ -28,58 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.firstlabel = new System.Windows.Forms.Label();
-            this.firstbutton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // firstlabel
+            // groupBox1
             // 
-            this.firstlabel.AutoSize = true;
-            this.firstlabel.Location = new System.Drawing.Point(77, 106);
-            this.firstlabel.Name = "firstlabel";
-            this.firstlabel.Size = new System.Drawing.Size(44, 16);
-            this.firstlabel.TabIndex = 0;
-            this.firstlabel.Text = "label1";
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Enabled = false;
+            this.groupBox1.Location = new System.Drawing.Point(200, 120);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(604, 219);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
             // 
-            // firstbutton
+            // button1
             // 
-            this.firstbutton.Location = new System.Drawing.Point(298, 162);
-            this.firstbutton.Name = "firstbutton";
-            this.firstbutton.Size = new System.Drawing.Size(75, 23);
-            this.firstbutton.TabIndex = 1;
-            this.firstbutton.Text = "button1";
-            this.firstbutton.UseVisualStyleBackColor = true;
-            this.firstbutton.Click += new System.EventHandler(this.firstbutton_Click);
+            this.button1.Location = new System.Drawing.Point(94, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(278, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(457, 57);
-            this.textBox1.Multiline = true;
+            this.textBox1.Location = new System.Drawing.Point(281, 129);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(352, 113);
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 2;
-            this.textBox1.Text = "ilk görünen yazı";
             // 
             // mainform
             // 
             this.ClientSize = new System.Drawing.Size(1093, 416);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.firstbutton);
-            this.Controls.Add(this.firstlabel);
+            this.Controls.Add(this.groupBox1);
             this.Name = "mainform";
             this.Load += new System.EventHandler(this.mainform_Load);
             this.Click += new System.EventHandler(this.mainform_Click);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label firstlabel;
-        private System.Windows.Forms.Button firstbutton;
+        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 

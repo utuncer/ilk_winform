f292b4ff5117cb070f9c7224400c3754ffa1fa21
Umut Tunçer <utuncerx@gmail.com>
2024-11-22 16:37:13 +0300

    Yazılan Sayıların Aralığındaki Sayılardan Seçilen Sayının Katlarını Yazan Uygulama

--- ilk winform/Form1.Designer.cs
+++ ilk winform/Form1.Designer.cs
@@ -28,45 +28,162 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.soyadBox = new System.Windows.Forms.TextBox();
-            this.adBox = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainform));
+            this.labelFrom = new System.Windows.Forms.Label();
+            this.labelTo = new System.Windows.Forms.Label();
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.buttonCount = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.labelDivisibleTerm = new System.Windows.Forms.Label();
+            this.cmBoxDivisibleTerm = new System.Windows.Forms.ComboBox();
+            this.txtDivisibleNumber = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.ChItalic = new System.Windows.Forms.CheckBox();
+            this.ChBold = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // soyadBox
+            // labelFrom
             // 
-            this.soyadBox.Location = new System.Drawing.Point(168, 59);
-            this.soyadBox.Name = "soyadBox";
-            this.soyadBox.Size = new System.Drawing.Size(100, 22);
-            this.soyadBox.TabIndex = 0;
+            resources.ApplyResources(this.labelFrom, "labelFrom");
+            this.labelFrom.Name = "labelFrom";
             // 
-            // adBox
+            // labelTo
             // 
-            this.adBox.Location = new System.Drawing.Point(42, 59);
-            this.adBox.Name = "adBox";
-            this.adBox.Size = new System.Drawing.Size(100, 22);
-            this.adBox.TabIndex = 1;
+            resources.ApplyResources(this.labelTo, "labelTo");
+            this.labelTo.Name = "labelTo";
             // 
-            // button1
+            // txtFrom
             // 
-            this.button1.Location = new System.Drawing.Point(297, 59);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click_2);
+            resources.ApplyResources(this.txtFrom, "txtFrom");
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtFrom_KeyPress);
+            // 
+            // txtTo
+            // 
+            resources.ApplyResources(this.txtTo, "txtTo");
+            this.txtTo.Name = "txtTo";
+            this.txtTo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtTo_KeyPress);
+            // 
+            // buttonCount
+            // 
+            resources.ApplyResources(this.buttonCount, "buttonCount");
+            this.buttonCount.Name = "buttonCount";
+            this.buttonCount.UseVisualStyleBackColor = true;
+            this.buttonCount.Click += new System.EventHandler(this.buttonCount_Click);
+            // 
+            // buttonExit
+            // 
+            resources.ApplyResources(this.buttonExit, "buttonExit");
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // labelDivisibleTerm
+            // 
+            resources.ApplyResources(this.labelDivisibleTerm, "labelDivisibleTerm");
+            this.labelDivisibleTerm.Name = "labelDivisibleTerm";
+            this.labelDivisibleTerm.Click += new System.EventHandler(this.labelDivisibleTerm_Click);
+            // 
+            // cmBoxDivisibleTerm
+            // 
+            this.cmBoxDivisibleTerm.FormattingEnabled = true;
+            resources.ApplyResources(this.cmBoxDivisibleTerm, "cmBoxDivisibleTerm");
+            this.cmBoxDivisibleTerm.Name = "cmBoxDivisibleTerm";
+            this.cmBoxDivisibleTerm.SelectedIndexChanged += new System.EventHandler(this.cmBoxDivisibleTerm_SelectedIndexChanged);
+            // 
+            // txtDivisibleNumber
+            // 
+            resources.ApplyResources(this.txtDivisibleNumber, "txtDivisibleNumber");
+            this.txtDivisibleNumber.Name = "txtDivisibleNumber";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // radioButton1
+            // 
+            resources.ApplyResources(this.radioButton1, "radioButton1");
+            this.radioButton1.Checked = true;
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            resources.ApplyResources(this.radioButton2, "radioButton2");
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            resources.ApplyResources(this.radioButton3, "radioButton3");
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            resources.ApplyResources(this.radioButton4, "radioButton4");
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // ChItalic
+            // 
+            resources.ApplyResources(this.ChItalic, "ChItalic");
+            this.ChItalic.Name = "ChItalic";
+            this.ChItalic.UseVisualStyleBackColor = true;
+            this.ChItalic.CheckedChanged += new System.EventHandler(this.ChItalic_CheckedChanged);
+            // 
+            // ChBold
+            // 
+            resources.ApplyResources(this.ChBold, "ChBold");
+            this.ChBold.Name = "ChBold";
+            this.ChBold.UseVisualStyleBackColor = true;
+            this.ChBold.CheckedChanged += new System.EventHandler(this.ChBold_CheckedChanged);
             // 
             // mainform
             // 
-            this.ClientSize = new System.Drawing.Size(1093, 416);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.adBox);
-            this.Controls.Add(this.soyadBox);
+            resources.ApplyResources(this, "$this");
+            this.Controls.Add(this.ChBold);
+            this.Controls.Add(this.ChItalic);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtDivisibleNumber);
+            this.Controls.Add(this.cmBoxDivisibleTerm);
+            this.Controls.Add(this.labelDivisibleTerm);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonCount);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.txtFrom);
+            this.Controls.Add(this.labelTo);
+            this.Controls.Add(this.labelFrom);
             this.Name = "mainform";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Load += new System.EventHandler(this.mainform_Load);
-            this.Click += new System.EventHandler(this.mainform_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -74,9 +191,23 @@
 
         #endregion
 
-        public System.Windows.Forms.TextBox soyadBox;
-        public System.Windows.Forms.TextBox adBox;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelFrom;
+        private System.Windows.Forms.Label labelTo;
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.Button buttonCount;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Label labelDivisibleTerm;
+        private System.Windows.Forms.ComboBox cmBoxDivisibleTerm;
+        private System.Windows.Forms.TextBox txtDivisibleNumber;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.CheckBox ChBold;
+        private System.Windows.Forms.CheckBox ChItalic;
     }
 }
 
--- ilk winform/Form1.cs
+++ ilk winform/Form1.cs
@@ -13,107 +13,153 @@ namespace ilk_winform
 {
     public partial class mainform : Form
     {
+        int ilkSayi, ikinciSayi, divisibleNumb, controlNumber = 0;
+        string disibleText = "";
+
         public mainform()
         {
             InitializeComponent();
         }
-
         private void mainform_Load(object sender, EventArgs e)
         {
-            //// MessageBox.Show("Form Yüklendi");
-            //comboBox1.Items.Add("İstanbul");
-            //comboBox1.Items.Add("Bursa");
-            //comboBox1.Items.Add("Ankara");
-            ////comboBox1.SelectedIndex = 2;
-            //button1.Enabled = false;
+            cmBoxDivisibleTerm.Text = "Select";
+            cmBoxDivisibleTerm.Items.Add("2");
+            cmBoxDivisibleTerm.Items.Add("3");
+            cmBoxDivisibleTerm.Items.Add("4");
+            cmBoxDivisibleTerm.Items.Add("5");
+            cmBoxDivisibleTerm.Items.Add("6");
+            cmBoxDivisibleTerm.Items.Add("7");
+            cmBoxDivisibleTerm.Items.Add("8");
+            cmBoxDivisibleTerm.Items.Add("9");
+            cmBoxDivisibleTerm.Items.Add("10");
+            txtDivisibleNumber.Enabled = true;
         }
 
-        private void mainform_Click(object sender, EventArgs e)
+        private void txtFrom_KeyPress(object sender, KeyPressEventArgs e)
         {
-            #region Basit Etkileşimli Mesaj Kutusu
-            //string message = "Bilinmeyen bir hata oluştu.";
-            //DialogResult result = MessageBox.Show(message, "Hata !!", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
-            //if (result == DialogResult.Yes)
-            //{
-            //    MessageBox.Show("evete basıldı");
-            //}
-            //if (result == DialogResult.No)
-            //{
-            //    MessageBox.Show("hayıra basıldı");
+            // Sadece sayılara izin verir (ve backspace'e)
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true; // Harfi engelle
 
-            //} 
-            #endregion
+            }
         }
-        private void firstbutton_Click(object sender, EventArgs e)
+        private void txtTo_KeyPress(object sender, KeyPressEventArgs e)
         {
-            #region Butona Tıkladığında Yazıyı (Label'ı) Düzenleyen Komut
-            //firstlabel.Text = "deneme";
-            //firstlabel.BackColor = Color.IndianRed;
-            //firstlabel.ForeColor = Color.White;
-            #endregion
+            // Sadece sayılara izin verir (ve backspace'e)
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true; // Harfi engelle
 
-            #region Textbox'a Yazılan Metni Label'a Yansıtan Kod
-            //firstlabel.Text = textBox1.Text; 
-            #endregion
+            }
         }
-        private void button1_Click(object sender, EventArgs e)
+
+        private void labelDivisibleTerm_Click(object sender, EventArgs e)
         {
-            #region Butonun Seçilip Seçilmediğini Kontrol Etme
-            //if (radioButton1.Checked == true)
-            //{
-            //    MessageBox.Show("Radio butonu seçilmiş");
-            //}
-            //else
-            //{
-            //    MessageBox.Show("Radio butonu seçilmemiş");
-            //} 
-            #endregion
+
         }
+
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
-            #region Butonun Değeri Değiştirildiğinde çıkacak Mesaj
-            //MessageBox.Show("Radio butonu değiştirildi"); 
-            #endregion
-        }
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-         #region ComboBox'tan Verilerden Birinin Seçili Seçilmediğine Göre Buton Aktifliğini Değiştiren Komut
-            //if (comboBox1.SelectedIndex == -1)
-            //{
-            //    button1.Enabled = false;
-            //}
-            //else
-            //{
-            //    button1.Enabled = true;
-            //} 
-            #endregion
-            #region ComboBox Verilerinden biri seçildiğinde seçilen değeri stringe çevirip labela atan komut
-            /* 
-     label2.Text = comboBox1.SelectedItem.ToString();
-     */
-            #endregion
+            if (radioButton1.Checked)
+            {
+                txtDivisibleNumber.ForeColor = Color.Black; 
+            }
+            
         }
 
-        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
         {
-            #region Tarih Seçtikten Sonra Yıl Ekleme/Çıkarma, Seçilen Tarihi Gösterme Kodu 
-            //MessageBox.Show($" {dateTimePicker1.Value.ToShortDateString()}");
-            //label1.Text = dateTimePicker1.Value.AddYears(2).ToShortDateString();
-            //label2.Text = dateTimePicker1.Value.AddYears(-2).ToShortDateString(); 
-            #endregion
+            if (radioButton2.Checked)
+            {
+                txtDivisibleNumber.ForeColor = Color.Red;
+            }
         }
-        private void button1_Click_2(object sender, EventArgs e)
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
         {
-            #region Tıklandığında Diğer Formada Ulaşan Basit Bir Kod
-            //string ad = adBox.Text;
-            //string soyad = soyadBox.Text;
-            //string adsoyad = ad + " " + soyad;
-            //Form2 form2goster = new Form2();
-            //form2goster.label1.Text = adsoyad;
-            //// açtığında önceki açılan formda etkileşime izin verir
-            //// form2goster.Show();
-            //form2goster.ShowDialog(); 
-            #endregion
+            if (radioButton3.Checked)
+            {
+                txtDivisibleNumber.ForeColor = Color.Green;
+            }
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton4.Checked)
+            {
+                txtDivisibleNumber.ForeColor = Color.Blue;
+            }
+        }
+
+        private void ChBold_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ChBold.Checked)
+            {
+                ChItalic.Checked = false;
+                txtDivisibleNumber.Font = new Font(Font, FontStyle.Bold);
+            }
+            else
+            {
+                txtDivisibleNumber.Font = new Font(Font, FontStyle.Regular);
+            }
+        }
+
+        private void ChItalic_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ChItalic.Checked)
+            {
+                ChBold.Checked = false;
+                txtDivisibleNumber.Font = new Font(Font, FontStyle.Italic);
+            }
+            else
+            {
+                txtDivisibleNumber.Font = new Font(Font, FontStyle.Regular);
+            }
+        }
+
+        private void cmBoxDivisibleTerm_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            divisibleNumb = Convert.ToInt32(cmBoxDivisibleTerm.SelectedItem.ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonCount_Click(object sender, EventArgs e)
+        {
+            if (txtFrom.Text.Trim() == "" || txtTo.Text.Trim() == "")
+            {
+                errorProvider1.SetError(txtFrom, "Please Fill TextBox");
+                MessageBox.Show("Please Fill Neccessary Fields");
+            }
+            else if (cmBoxDivisibleTerm.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please Select Divisible Term");
+            }
+            else
+            {
+                disibleText = "";
+                controlNumber = 0;
+                ilkSayi = Convert.ToInt32(txtFrom.Text);
+                ikinciSayi = Convert.ToInt32(txtTo.Text);
+                for (int i = ilkSayi; i <= ikinciSayi; i++)
+                {
+                    if (i % divisibleNumb == 0)
+                    {
+                        disibleText += i + " ";
+                        controlNumber++;
+                        if (controlNumber % 20 == 0)
+                        {
+                            disibleText += Environment.NewLine;
+                        }
+                    }
+                }
+                txtDivisibleNumber.Text = disibleText;
+            }
+
         }
     }
 }

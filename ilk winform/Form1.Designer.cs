05ea03a81506e071fe020322905cb1775b3cb2fc
Umut Tunçer <utuncerx@gmail.com>
2024-11-22 18:53:53 +0300

    Methodların Basit Mantığı

--- ilk winform/Form1.Designer.cs
+++ ilk winform/Form1.Designer.cs
@@ -31,71 +31,181 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainform));
             this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
-            this.button1 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.sayi = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.sayiKaresi = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.sayiKup = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnGizle = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.btnYaziTasi = new System.Windows.Forms.Button();
+            this.btnLabelYaz = new System.Windows.Forms.Button();
+            this.btnMetniDegistir = new System.Windows.Forms.Button();
+            this.btnTopla = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // errorProvider1
             // 
             this.errorProvider1.ContainerControl = this;
             // 
-            // button1
+            // btnGizle
             // 
-            resources.ApplyResources(this.button1, "button1");
-            this.button1.Name = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            resources.ApplyResources(this.btnGizle, "btnGizle");
+            this.btnGizle.Name = "btnGizle";
+            this.btnGizle.UseVisualStyleBackColor = true;
+            this.btnGizle.Click += new System.EventHandler(this.btnGizle_Click);
             // 
-            // dataGridView1
+            // label1
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.sayi,
-            this.sayiKaresi,
-            this.sayiKup});
-            resources.ApplyResources(this.dataGridView1, "dataGridView1");
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 24;
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
             // 
-            // sayi
+            // textBox1
             // 
-            resources.ApplyResources(this.sayi, "sayi");
-            this.sayi.Name = "sayi";
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
             // 
-            // sayiKaresi
+            // textBox2
             // 
-            resources.ApplyResources(this.sayiKaresi, "sayiKaresi");
-            this.sayiKaresi.Name = "sayiKaresi";
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
             // 
-            // sayiKup
+            // groupBox1
             // 
-            resources.ApplyResources(this.sayiKup, "sayiKup");
-            this.sayiKup.Name = "sayiKup";
+            this.groupBox1.Controls.Add(this.radioButton6);
+            this.groupBox1.Controls.Add(this.radioButton5);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // radioButton6
+            // 
+            resources.ApplyResources(this.radioButton6, "radioButton6");
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.TabStop = true;
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            resources.ApplyResources(this.radioButton5, "radioButton5");
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.TabStop = true;
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            resources.ApplyResources(this.radioButton4, "radioButton4");
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.TabStop = true;
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            resources.ApplyResources(this.radioButton3, "radioButton3");
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.TabStop = true;
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            resources.ApplyResources(this.radioButton2, "radioButton2");
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.TabStop = true;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            resources.ApplyResources(this.radioButton1, "radioButton1");
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // btnTemizle
+            // 
+            resources.ApplyResources(this.btnTemizle, "btnTemizle");
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.UseVisualStyleBackColor = true;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // btnYaziTasi
+            // 
+            resources.ApplyResources(this.btnYaziTasi, "btnYaziTasi");
+            this.btnYaziTasi.Name = "btnYaziTasi";
+            this.btnYaziTasi.UseVisualStyleBackColor = true;
+            this.btnYaziTasi.Click += new System.EventHandler(this.btnYaziTasi_Click);
+            // 
+            // btnLabelYaz
+            // 
+            resources.ApplyResources(this.btnLabelYaz, "btnLabelYaz");
+            this.btnLabelYaz.Name = "btnLabelYaz";
+            this.btnLabelYaz.UseVisualStyleBackColor = true;
+            this.btnLabelYaz.Click += new System.EventHandler(this.btnLabelYaz_Click);
+            // 
+            // btnMetniDegistir
+            // 
+            resources.ApplyResources(this.btnMetniDegistir, "btnMetniDegistir");
+            this.btnMetniDegistir.Name = "btnMetniDegistir";
+            this.btnMetniDegistir.UseVisualStyleBackColor = true;
+            this.btnMetniDegistir.Click += new System.EventHandler(this.btnMetniDegistir_Click);
+            // 
+            // btnTopla
+            // 
+            resources.ApplyResources(this.btnTopla, "btnTopla");
+            this.btnTopla.Name = "btnTopla";
+            this.btnTopla.UseVisualStyleBackColor = true;
+            this.btnTopla.Click += new System.EventHandler(this.btnTopla_Click);
             // 
             // mainform
             // 
             resources.ApplyResources(this, "$this");
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnTopla);
+            this.Controls.Add(this.btnMetniDegistir);
+            this.Controls.Add(this.btnLabelYaz);
+            this.Controls.Add(this.btnYaziTasi);
+            this.Controls.Add(this.btnTemizle);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGizle);
             this.Name = "mainform";
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.ErrorProvider errorProvider1;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn sayi;
-        private System.Windows.Forms.DataGridViewTextBoxColumn sayiKaresi;
-        private System.Windows.Forms.DataGridViewTextBoxColumn sayiKup;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnGizle;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLabelYaz;
+        private System.Windows.Forms.Button btnYaziTasi;
+        private System.Windows.Forms.Button btnTemizle;
+        private System.Windows.Forms.Button btnMetniDegistir;
+        private System.Windows.Forms.Button btnTopla;
     }
 }
 
--- ilk winform/Form1.cs
+++ ilk winform/Form1.cs
@@ -17,18 +17,76 @@ namespace ilk_winform
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        //değer döndürmeyen metod
+        public void rdGizle()
         {
-            dataGridView1.Rows.Clear();
-            for (int i = 0; i < 10; i++)
+            if (groupBox1.Visible == true)
             {
-                int sayininKaresi = i * i;
-                int sayininKupu = i * i * i;
-
-                dataGridView1.Rows.Add(i.ToString(),sayininKaresi.ToString(),sayininKupu.ToString());
+                btnGizle.Text = "Butonları Gör";
+                groupBox1.Visible = false;
             }
+            else
+            {
+                btnGizle.Text = "Butonları Gizle";
+                groupBox1.Visible = true;
+            }
+        }
+        //değer döndüren metod
+        public string yaziTasi()
+        {
+            string yazi = textBox1.Text;
+            textBox1.Clear();
+            return yazi;
+        }
+        //değer döndürmeyen metod
+        public void yaziTemizle()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+        //public kullanılmadığında varsayılan private'tir
+        string labelaYaz()
+        {
+            string metin = textBox1.Text;
+            return metin;
+        }
+        public void metniKopyala(string metin)
+        {
+            textBox1.Text = metin;
+        }
+        public int topla(int sayi1, int sayi2)
+        {
+            int toplam = sayi1 + sayi2;
+            return toplam;
+        }
+        private void btnGizle_Click(object sender, EventArgs e)
+        {
+            rdGizle();
+        }
 
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            yaziTemizle();
+        }
+
+        private void btnYaziTasi_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = yaziTasi();
+        }
+
+        private void btnLabelYaz_Click(object sender, EventArgs e)
+        {
+            label1.Text = labelaYaz();
+        }
+
+        private void btnMetniDegistir_Click(object sender, EventArgs e)
+        {
+            metniKopyala(textBox2.Text);
+        }
+
+        private void btnTopla_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(topla(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text)).ToString());
         }
     }
 }

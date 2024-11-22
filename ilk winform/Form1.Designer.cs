59ed6f812e3caf615b9de1be6ce67a453babaa48
Umut Tunçer <utuncerx@gmail.com>
2024-11-22 22:01:10 +0300

    Random Sayılarla Rastgele Sayılar ve Rastgele Şifre Üreten Uygulama

--- ilk winform/Form1.Designer.cs
+++ ilk winform/Form1.Designer.cs
@@ -29,49 +29,79 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainform));
-            this.btnFillList = new System.Windows.Forms.Button();
-            this.txtList = new System.Windows.Forms.TextBox();
-            this.cmbDays = new System.Windows.Forms.ComboBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // btnFillList
+            // button1
             // 
-            resources.ApplyResources(this.btnFillList, "btnFillList");
-            this.btnFillList.Name = "btnFillList";
-            this.btnFillList.UseVisualStyleBackColor = true;
-            this.btnFillList.Click += new System.EventHandler(this.btnFillList_Click);
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // txtList
+            // label1
             // 
-            resources.ApplyResources(this.txtList, "txtList");
-            this.txtList.Name = "txtList";
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
             // 
-            // cmbDays
+            // label2
             // 
-            resources.ApplyResources(this.cmbDays, "cmbDays");
-            this.cmbDays.FormattingEnabled = true;
-            this.cmbDays.Name = "cmbDays";
-            this.cmbDays.SelectedIndexChanged += new System.EventHandler(this.cmbDays_SelectedIndexChanged);
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
             // 
-            // dataGridView1
+            // label3
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            resources.ApplyResources(this.dataGridView1, "dataGridView1");
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 24;
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // mainform
             // 
             resources.ApplyResources(this, "$this");
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.cmbDays);
-            this.Controls.Add(this.txtList);
-            this.Controls.Add(this.btnFillList);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Name = "mainform";
-            this.Load += new System.EventHandler(this.mainform_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -79,10 +109,15 @@
 
         #endregion
 
-        private System.Windows.Forms.Button btnFillList;
-        private System.Windows.Forms.TextBox txtList;
-        private System.Windows.Forms.ComboBox cmbDays;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- ilk winform/Form1.cs
+++ ilk winform/Form1.cs
@@ -18,103 +18,31 @@ namespace ilk_winform
         {
             InitializeComponent();
         }
-        List<int> numberList = new List<int>();
-        List<string> textList = new List<string>();
-        List<Employee> list = new List<Employee>();
-        List<Days> daylist = new List<Days>();
-        private void mainform_Load(object sender, EventArgs e)
+
+        private void button1_Click(object sender, EventArgs e)
         {
-            Employee employee1 = new Employee();
-            employee1.Name = "Name1";
-            employee1.Surname = "Surname";
-            employee1.EmployeeId = 1;
-
-            Employee employee2 = new Employee();
-            employee2.Name = "Name2";
-            employee2.Surname = "Surname2";
-            employee2.EmployeeId = 2;
-
-            Employee employee3 = new Employee();
-            employee3.Name = "Name3";
-            employee3.Surname = "Surname3";
-            employee3.EmployeeId = 3;
-
-            list.Add(employee1);
-            list.Add(employee2);
-            list.Add(employee3);
-
-
-
-            Days day1 = new Days();
-            day1.Id = 1;
-            day1.textValue = "Monday";
-
-            Days day2 = new Days();
-            day2.Id = 2;
-            day2.textValue = "Tuesday";
-
-            Days day3 = new Days();
-            day3.Id = 3;
-            day3.textValue = "Wednasday";
-
-            Days day4 = new Days();
-            day4.Id = 4;
-            day4.textValue = "Thursday";
-
-            Days day5 = new Days();
-            day5.Id = 5;
-            day5.textValue = "Friday";
-
-            Days day6 = new Days();
-            day6.Id = 6;
-            day6.textValue = "Saturday";
-
-            Days day7 = new Days();
-            day7.Id = 7;
-            day7.textValue = "Sunday";
-
-            daylist.Add(day1);
-            daylist.Add(day2);
-            daylist.Add(day3);
-            daylist.Add(day4);
-            daylist.Add(day5);
-            daylist.Add(day6);
-            daylist.Add(day7);
-
-            //comboBox'a kaynak olarak listeyi gösteriyoruz
-            cmbDays.DataSource = daylist;
-            cmbDays.DisplayMember = "textValue";
-            cmbDays.ValueMember = "ID";
-            dataGridView1.DataSource = daylist;
-        }
-        void fillList()
-        {
-            numberList.Add(1);
-            numberList.Add(2);
-            numberList.Add(3);
-        }
-        private void btnFillList_Click(object sender, EventArgs e)
-        {
-            #region numberList'in verilerini textbox'ta görüntüleyen kod
-            //numberList.Clear();
-            //fillList();
-            //foreach (var item in numberList)
-            //{
-            //    txtList.Text += item.ToString() + Environment.NewLine;
-            //} 
-            #endregion
-            #region Sınıftan oluşturulan objelerin verilerini textbox'ta görüntüleyen kod
-            //foreach (var item in list)
-            //{
-            //    txtList.Text += item.EmployeeId + " " + item.Name + " " + item.Surname + Environment.NewLine; 
-            //} 
-            #endregion
-
+            Random rnd = new Random();
+            byte[] Array = new byte[7];
+            rnd.NextBytes(Array);
+            label1.Text = Array[0].ToString();
+            label2.Text = Array[1].ToString();
+            label3.Text = Array[2].ToString();
+            label4.Text = Array[3].ToString();
+            label5.Text = Array[4].ToString();
+            label6.Text = Array[5].ToString();
+            label7.Text = Array[6].ToString();
         }
 
-        private void cmbDays_SelectedIndexChanged(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
-          //  MessageBox.Show(cmbDays.SelectedValue.ToString());
+            string allValid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;':,.<>?/";
+            string result = " ";
+            Random rnd2 = new Random();
+            for (int i = 0; i < 6; i++)
+            {
+                result += allValid[rnd2.Next(0, Convert.ToInt32(allValid.Length))];
+            }
+            label1.Text = result;
         }
     }
 }

4d2c27994e366aa6d067db62ab43f945f2ad8629
Umut Tunçer <utuncerx@gmail.com>
2024-11-23 17:41:53 +0300

    Math Metodlarının Kullanıldığı Basit Hesap Makinesi Uygulaması

--- ilk winform/Form1.Designer.cs
+++ ilk winform/Form1.Designer.cs
@@ -28,65 +28,203 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainform));
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.btnStart = new System.Windows.Forms.Button();
-            this.btnStop = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnCopyFirst = new System.Windows.Forms.Button();
+            this.btnExp = new System.Windows.Forms.Button();
+            this.btnTan = new System.Windows.Forms.Button();
+            this.btnCos = new System.Windows.Forms.Button();
+            this.btnSin = new System.Windows.Forms.Button();
+            this.btnLog1 = new System.Windows.Forms.Button();
+            this.btnLog = new System.Windows.Forms.Button();
+            this.btnSqrt = new System.Windows.Forms.Button();
+            this.btnPow = new System.Windows.Forms.Button();
+            this.btnSign = new System.Windows.Forms.Button();
+            this.btnAbs = new System.Windows.Forms.Button();
+            this.btnMax = new System.Windows.Forms.Button();
+            this.btnMin = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSecondNumber = new System.Windows.Forms.TextBox();
+            this.txtFirstNumber = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // timer1
+            // groupBox1
             // 
-            this.timer1.Interval = 1000;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            this.groupBox1.Controls.Add(this.btnCopyFirst);
+            this.groupBox1.Controls.Add(this.btnExp);
+            this.groupBox1.Controls.Add(this.btnTan);
+            this.groupBox1.Controls.Add(this.btnCos);
+            this.groupBox1.Controls.Add(this.btnSin);
+            this.groupBox1.Controls.Add(this.btnLog1);
+            this.groupBox1.Controls.Add(this.btnLog);
+            this.groupBox1.Controls.Add(this.btnSqrt);
+            this.groupBox1.Controls.Add(this.btnPow);
+            this.groupBox1.Controls.Add(this.btnSign);
+            this.groupBox1.Controls.Add(this.btnAbs);
+            this.groupBox1.Controls.Add(this.btnMax);
+            this.groupBox1.Controls.Add(this.btnMin);
+            this.groupBox1.Controls.Add(this.btnClear);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
             // 
-            // btnStart
+            // btnCopyFirst
             // 
-            resources.ApplyResources(this.btnStart, "btnStart");
-            this.btnStart.Name = "btnStart";
-            this.btnStart.UseVisualStyleBackColor = true;
-            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            resources.ApplyResources(this.btnCopyFirst, "btnCopyFirst");
+            this.btnCopyFirst.Name = "btnCopyFirst";
+            this.btnCopyFirst.UseVisualStyleBackColor = true;
+            this.btnCopyFirst.Click += new System.EventHandler(this.btnCopyFirst_Click);
             // 
-            // btnStop
+            // btnExp
             // 
-            resources.ApplyResources(this.btnStop, "btnStop");
-            this.btnStop.Name = "btnStop";
-            this.btnStop.UseVisualStyleBackColor = true;
-            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            resources.ApplyResources(this.btnExp, "btnExp");
+            this.btnExp.Name = "btnExp";
+            this.btnExp.UseVisualStyleBackColor = true;
+            this.btnExp.Click += new System.EventHandler(this.btnExp_Click);
+            // 
+            // btnTan
+            // 
+            resources.ApplyResources(this.btnTan, "btnTan");
+            this.btnTan.Name = "btnTan";
+            this.btnTan.UseVisualStyleBackColor = true;
+            this.btnTan.Click += new System.EventHandler(this.btnTan_Click);
+            // 
+            // btnCos
+            // 
+            resources.ApplyResources(this.btnCos, "btnCos");
+            this.btnCos.Name = "btnCos";
+            this.btnCos.UseVisualStyleBackColor = true;
+            this.btnCos.Click += new System.EventHandler(this.btnCos_Click);
+            // 
+            // btnSin
+            // 
+            resources.ApplyResources(this.btnSin, "btnSin");
+            this.btnSin.Name = "btnSin";
+            this.btnSin.UseVisualStyleBackColor = true;
+            this.btnSin.Click += new System.EventHandler(this.btnSin_Click);
+            // 
+            // btnLog1
+            // 
+            resources.ApplyResources(this.btnLog1, "btnLog1");
+            this.btnLog1.Name = "btnLog1";
+            this.btnLog1.UseVisualStyleBackColor = true;
+            this.btnLog1.Click += new System.EventHandler(this.btnLog1_Click);
+            // 
+            // btnLog
+            // 
+            resources.ApplyResources(this.btnLog, "btnLog");
+            this.btnLog.Name = "btnLog";
+            this.btnLog.UseVisualStyleBackColor = true;
+            this.btnLog.Click += new System.EventHandler(this.btnLog_Click);
+            // 
+            // btnSqrt
+            // 
+            resources.ApplyResources(this.btnSqrt, "btnSqrt");
+            this.btnSqrt.Name = "btnSqrt";
+            this.btnSqrt.UseVisualStyleBackColor = true;
+            this.btnSqrt.Click += new System.EventHandler(this.btnSqrt_Click);
+            // 
+            // btnPow
+            // 
+            resources.ApplyResources(this.btnPow, "btnPow");
+            this.btnPow.Name = "btnPow";
+            this.btnPow.UseVisualStyleBackColor = true;
+            this.btnPow.Click += new System.EventHandler(this.btnPow_Click);
+            // 
+            // btnSign
+            // 
+            resources.ApplyResources(this.btnSign, "btnSign");
+            this.btnSign.Name = "btnSign";
+            this.btnSign.UseVisualStyleBackColor = true;
+            this.btnSign.Click += new System.EventHandler(this.btnSign_Click);
+            // 
+            // btnAbs
+            // 
+            resources.ApplyResources(this.btnAbs, "btnAbs");
+            this.btnAbs.Name = "btnAbs";
+            this.btnAbs.UseVisualStyleBackColor = true;
+            this.btnAbs.Click += new System.EventHandler(this.btnAbs_Click);
+            // 
+            // btnMax
+            // 
+            resources.ApplyResources(this.btnMax, "btnMax");
+            this.btnMax.Name = "btnMax";
+            this.btnMax.UseVisualStyleBackColor = true;
+            this.btnMax.Click += new System.EventHandler(this.btnMax_Click);
+            // 
+            // btnMin
+            // 
+            resources.ApplyResources(this.btnMin, "btnMin");
+            this.btnMin.Name = "btnMin";
+            this.btnMin.UseVisualStyleBackColor = true;
+            this.btnMin.Click += new System.EventHandler(this.btnMin_Click);
+            // 
+            // btnClear
+            // 
+            resources.ApplyResources(this.btnClear, "btnClear");
+            this.btnClear.Name = "btnClear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
             // label1
             // 
             resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
             // 
-            // progressBar1
+            // label2
             // 
-            resources.ApplyResources(this.progressBar1, "progressBar1");
-            this.progressBar1.Maximum = 60;
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Value = 60;
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
             // 
-            // button1
+            // label3
             // 
-            this.button1.BackColor = System.Drawing.Color.White;
-            resources.ApplyResources(this.button1, "button1");
-            this.button1.Name = "button1";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // txtSecondNumber
+            // 
+            resources.ApplyResources(this.txtSecondNumber, "txtSecondNumber");
+            this.txtSecondNumber.Name = "txtSecondNumber";
+            this.txtSecondNumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSecondNumber_KeyPress);
+            // 
+            // txtFirstNumber
+            // 
+            resources.ApplyResources(this.txtFirstNumber, "txtFirstNumber");
+            this.txtFirstNumber.Name = "txtFirstNumber";
+            this.txtFirstNumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtFirstNumber_KeyPress);
+            // 
+            // txtResult
+            // 
+            resources.ApplyResources(this.txtResult, "txtResult");
+            this.txtResult.Name = "txtResult";
+            // 
+            // btnExit
+            // 
+            resources.ApplyResources(this.btnExit, "btnExit");
+            this.btnExit.Name = "btnExit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // mainform
             // 
             resources.ApplyResources(this, "$this");
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtFirstNumber);
+            this.Controls.Add(this.txtSecondNumber);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnStop);
-            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.groupBox1);
             this.Name = "mainform";
             this.Load += new System.EventHandler(this.mainform_Load);
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -94,12 +232,28 @@
 
         #endregion
 
-        private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Button btnStart;
-        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnCopyFirst;
+        private System.Windows.Forms.Button btnExp;
+        private System.Windows.Forms.Button btnTan;
+        private System.Windows.Forms.Button btnCos;
+        private System.Windows.Forms.Button btnSin;
+        private System.Windows.Forms.Button btnLog1;
+        private System.Windows.Forms.Button btnLog;
+        private System.Windows.Forms.Button btnSqrt;
+        private System.Windows.Forms.Button btnPow;
+        private System.Windows.Forms.Button btnAbs;
+        private System.Windows.Forms.Button btnMax;
+        private System.Windows.Forms.Button btnMin;
+        private System.Windows.Forms.Button btnClear;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSecondNumber;
+        private System.Windows.Forms.TextBox txtFirstNumber;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnSign;
+        private System.Windows.Forms.Button btnExit;
     }
 }
 
--- ilk winform/Form1.cs
+++ ilk winform/Form1.cs
@@ -14,55 +14,303 @@ namespace ilk_winform
 {
     public partial class mainform : Form
     {
-        // Timer Class
-        // Interval -> milisaniye'yi ifade eder 5 saniye = 5000 milisaniye
+        // Math Sınıfı
+        // Math metotları -> https://prnt.sc/QBFF_LNcu6sF
         public mainform()
         {
             InitializeComponent();
         }
-        int counter = 60;
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            //MessageBox.Show("Timer Aktive");
-            progressBar1.Value = counter;
-            if(counter == 0)
-            {
-                timer1.Stop();
-                MessageBox.Show("Timer was stopped");
-            }
-            else
-            {
-                counter--;
-            }
-            label1.Text = counter.ToString();
-            if(counter % 2 == 0)
-            {
-                button1.BackColor = Color.White;
-            }
-            else
-            {
-                button1.BackColor = Color.Red;
-            }
-        }
-
-        private void btnStart_Click(object sender, EventArgs e)
-        {
-            timer1.Start();
-        }
-
-        private void btnStop_Click(object sender, EventArgs e)
-        {
-            timer1.Stop();
-        }
-
+        //text box'ın boş olup olmadığını konrol etmek için
+        bool control = false;
+        //text boxtan gelen verileri tutmak için
+        double firstNumber = 0, secondNumber = 0;
         private void mainform_Load(object sender, EventArgs e)
         {
-            label1.Text = 60.ToString();
-        }
 
-        private void button1_Click(object sender, EventArgs e)
+        }
+        void set1Parameter(string text)
         {
-
+            if (text.Trim() != "")
+            {
+                control = true;
+                firstNumber = Convert.ToDouble(text);
+            }
+            else
+            {
+                control = false;
+            }
         }
+        void set2Parameter(string text1, string text2)
+        {
+            if (text1.Trim() != "" && text2.Trim() != "")
+            {
+                control = true;
+                firstNumber = Convert.ToDouble(text1);
+                secondNumber = Convert.ToDouble(text2);
+            }
+            else
+            {
+                control = false;
+            }
+        }
+        private void txtFirstNumber_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            #region Sadece rakam yazma kodu
+            TextBox textBox = sender as TextBox;
+
+            // Sayı kontrolü, kontrol tuşları, nokta ve eksi işareti
+            if (!char.IsDigit(e.KeyChar) &&
+                !char.IsControl(e.KeyChar) &&
+                e.KeyChar != '.' &&
+                e.KeyChar != '-')
+            {
+                //e.Handled özelliği, olayın işlenip işlenmediğini belirler:
+                //true: Olay işlenmiş olarak kabul edilir ve varsayılan davranış engellenir.
+                //false: Olay işlenmemiştir ve varsayılan davranış devam eder.
+                e.Handled = true; // Geçersiz karakter engellendi
+            }
+
+            // Nokta birden fazla olamaz
+            if (e.KeyChar == '.' && textBox.Text.Contains("."))
+            {
+                e.Handled = true;
+            }
+
+            // Eksi işareti sadece başta olabilir
+            if (e.KeyChar == '-' && textBox.SelectionStart != 0)
+            {
+                e.Handled = true;
+            }
+            #endregion
+        }
+
+        private void txtSecondNumber_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            #region Sadece rakam yazma kodu
+            TextBox textBox = sender as TextBox;
+
+            // Sayı kontrolü, kontrol tuşları, nokta ve eksi işareti
+            if (!char.IsDigit(e.KeyChar) &&
+                !char.IsControl(e.KeyChar) &&
+                e.KeyChar != '.' &&
+                e.KeyChar != '-')
+            {
+                //e.Handled özelliği, olayın işlenip işlenmediğini belirler:
+                //true: Olay işlenmiş olarak kabul edilir ve varsayılan davranış engellenir.
+                //false: Olay işlenmemiştir ve varsayılan davranış devam eder.
+                e.Handled = true; // Geçersiz karakter engellendi
+            }
+
+            // Nokta birden fazla olamaz
+            if (e.KeyChar == '.' && textBox.Text.Contains("."))
+            {
+                e.Handled = true;
+            }
+
+            // Eksi işareti sadece başta olabilir
+            if (e.KeyChar == '-' && textBox.SelectionStart != 0)
+            {
+                e.Handled = true;
+            }
+            #endregion
+        }
+
+        private void btnMin_Click(object sender, EventArgs e)
+        {
+            set2Parameter(txtFirstNumber.Text, txtSecondNumber.Text);
+            if (!control)
+            {
+                MessageBox.Show("Please fill the Necessary Fields");
+            }
+            else
+            {
+                txtResult.Text = Math.Min(Convert.ToDecimal(firstNumber), Convert.ToDecimal(secondNumber)).ToString();
+            }
+        }
+
+        private void btnMax_Click(object sender, EventArgs e)
+        {
+            set2Parameter(txtFirstNumber.Text, txtSecondNumber.Text);
+            if (!control)
+            {
+                MessageBox.Show("Please fill the Necessary Fields");
+            }
+            else
+            {
+                // Sayının Mutlak Değerini Alır
+                txtResult.Text = Math.Max(Convert.ToDecimal(firstNumber), Convert.ToDecimal(secondNumber)).ToString();
+            }
+        }
+
+        private void btnAbs_Click(object sender, EventArgs e)
+        {
+            set1Parameter(txtFirstNumber.Text);
+            if (!control)
+            {
+                MessageBox.Show("Please fill the Necessary Fields");
+            }
+            else
+            {
+                txtResult.Text = Math.Abs(Convert.ToDecimal(firstNumber)).ToString();
+            }
+        }
+
+        private void btnSign_Click(object sender, EventArgs e)
+        {
+            set1Parameter(txtFirstNumber.Text);
+            if (!control)
+            {
+                MessageBox.Show("Please fill the Necessary Fields");
+            }
+            else
+            {
+                // Girilen sayısı pozitif ise 1, negatif ise -1 değerini döndürür
+                txtResult.Text = Math.Sign(Convert.ToDecimal(firstNumber)).ToString();
+            }
+        }
+
+        private void btnSin_Click(object sender, EventArgs e)
+        {
+            set1Parameter(txtFirstNumber.Text);
+            if (!control)
+            {
+                MessageBox.Show("Please fill the Necessary Fields");
+            }
+            else
+            {
+                // Derece cinsinden girilen sayıyı radyant'a çeviricez
+                double temp = (firstNumber * (Math.PI)) / 180;
+                txtResult.Text = Math.Sin(temp).ToString();
+            }
+        }
+
+        private void btnCos_Click(object sender, EventArgs e)
+        {
+            set1Parameter(txtFirstNumber.Text);
+            if (!control)
+            {
+                MessageBox.Show("Please fill the Necessary Fields");
+            }
+            else
+            {
+                // Derece cinsinden girilen sayıyı radyant'a çeviricez
+                double temp = (firstNumber * (Math.PI)) / 180;
+                txtResult.Text = Math.Cos(temp).ToString();
+            }
+        }
+
+        private void btnTan_Click(object sender, EventArgs e)
+        {
+            set1Parameter(txtFirstNumber.Text);
+            if (!control)
+            {
+                MessageBox.Show("Please fill the Necessary Fields");
+            }
+            else
+            {
+                // Derece cinsinden girilen sayıyı radyant'a çeviricez
+                double temp = (firstNumber * (Math.PI)) / 180;
+                txtResult.Text = Math.Tan(temp).ToString();
+            }
+        }
+
+        private void btnPow_Click(object sender, EventArgs e)
+        {
+            set2Parameter(txtFirstNumber.Text, txtSecondNumber.Text);
+            if (!control)
+            {
+                MessageBox.Show("Please fill the Necessary Fields");
+            }
+            else
+            {
+                // firstNumber^secondNumber işlemi
+                txtResult.Text = Math.Pow(firstNumber, secondNumber).ToString();
+            }
+        }
+
+        private void btnSqrt_Click(object sender, EventArgs e)
+        {
+            set1Parameter(txtFirstNumber.Text);
+            if (!control)
+            {
+                MessageBox.Show("Please fill the Necessary Fields");
+            }
+            else
+            {
+                // Girilen sayının karekökünü gösterir örneğin:
+                // firstNumber 36 girilmişse sonuç 6 gösterilir.
+                txtResult.Text = Math.Sqrt(firstNumber).ToString();
+            }
+        }
+
+        private void btnLog_Click(object sender, EventArgs e)
+        {
+            set2Parameter(txtFirstNumber.Text, txtSecondNumber.Text);
+            if (!control)
+            {
+                MessageBox.Show("Please fill the Necessary Fields");
+            }
+            else
+            {
+                // secondNumber tabanında firstNumber
+                // secondNumber = 10, firstNumber = 1000 cevap = 3
+                txtResult.Text = Math.Log(firstNumber, secondNumber).ToString();
+            }
+        }
+
+        private void btnLog1_Click(object sender, EventArgs e)
+        {
+            set1Parameter(txtFirstNumber.Text);
+            if (!control)
+            {
+                MessageBox.Show("Please fill the Necessary Fields");
+            }
+            else
+            {
+                // 10 tabanında firstNumber
+                // firstNumber = 1000, sonuç 3 
+                txtResult.Text = Math.Log10(firstNumber).ToString();
+            }
+        }
+
+        private void btnExp_Click(object sender, EventArgs e)
+        {
+            set1Parameter(txtFirstNumber.Text);
+            if (!control)
+            {
+                MessageBox.Show("Please fill the Necessary Fields");
+            }
+            else
+            {
+                // e tabanında firstNumber
+                txtResult.Text = Math.Exp(firstNumber).ToString();
+            }
+        }
+
+        private void btnCopyFirst_Click(object sender, EventArgs e)
+        {
+            if (txtResult.Text.Trim() != "")
+            {
+                txtFirstNumber.Text = txtResult.Text;
+                txtSecondNumber.Clear();
+                txtResult.Clear();
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            firstNumber = 0;
+            secondNumber = 0;
+            txtFirstNumber.Clear();
+            txtSecondNumber.Clear();
+            txtResult.Clear();
+        }
+
+
     }
 }

e1385ec623ba13d3605c3497dde79c0028da56f6
Umut Tunçer <utuncerx@gmail.com>
2024-11-24 18:04:40 +0300

    Kapsülleme ve Erişim Belirleyici Kullanılarak Formda Girilen Değerleri Diğer Forma Aktarma

--- ilk winform/Form1.Designer.cs
+++ ilk winform/Form1.Designer.cs
@@ -29,202 +29,77 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainform));
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.btnCopyFirst = new System.Windows.Forms.Button();
-            this.btnExp = new System.Windows.Forms.Button();
-            this.btnTan = new System.Windows.Forms.Button();
-            this.btnCos = new System.Windows.Forms.Button();
-            this.btnSin = new System.Windows.Forms.Button();
-            this.btnLog1 = new System.Windows.Forms.Button();
-            this.btnLog = new System.Windows.Forms.Button();
-            this.btnSqrt = new System.Windows.Forms.Button();
-            this.btnPow = new System.Windows.Forms.Button();
-            this.btnSign = new System.Windows.Forms.Button();
-            this.btnAbs = new System.Windows.Forms.Button();
-            this.btnMax = new System.Windows.Forms.Button();
-            this.btnMin = new System.Windows.Forms.Button();
-            this.btnClear = new System.Windows.Forms.Button();
+            this.txtID = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.txtYas = new System.Windows.Forms.TextBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.txtSecondNumber = new System.Windows.Forms.TextBox();
-            this.txtFirstNumber = new System.Windows.Forms.TextBox();
-            this.txtResult = new System.Windows.Forms.TextBox();
-            this.btnExit = new System.Windows.Forms.Button();
-            this.groupBox1.SuspendLayout();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPozisyon = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // txtID
             // 
-            this.groupBox1.Controls.Add(this.btnCopyFirst);
-            this.groupBox1.Controls.Add(this.btnExp);
-            this.groupBox1.Controls.Add(this.btnTan);
-            this.groupBox1.Controls.Add(this.btnCos);
-            this.groupBox1.Controls.Add(this.btnSin);
-            this.groupBox1.Controls.Add(this.btnLog1);
-            this.groupBox1.Controls.Add(this.btnLog);
-            this.groupBox1.Controls.Add(this.btnSqrt);
-            this.groupBox1.Controls.Add(this.btnPow);
-            this.groupBox1.Controls.Add(this.btnSign);
-            this.groupBox1.Controls.Add(this.btnAbs);
-            this.groupBox1.Controls.Add(this.btnMax);
-            this.groupBox1.Controls.Add(this.btnMin);
-            this.groupBox1.Controls.Add(this.btnClear);
-            resources.ApplyResources(this.groupBox1, "groupBox1");
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.TabStop = false;
-            // 
-            // btnCopyFirst
-            // 
-            resources.ApplyResources(this.btnCopyFirst, "btnCopyFirst");
-            this.btnCopyFirst.Name = "btnCopyFirst";
-            this.btnCopyFirst.UseVisualStyleBackColor = true;
-            this.btnCopyFirst.Click += new System.EventHandler(this.btnCopyFirst_Click);
-            // 
-            // btnExp
-            // 
-            resources.ApplyResources(this.btnExp, "btnExp");
-            this.btnExp.Name = "btnExp";
-            this.btnExp.UseVisualStyleBackColor = true;
-            this.btnExp.Click += new System.EventHandler(this.btnExp_Click);
-            // 
-            // btnTan
-            // 
-            resources.ApplyResources(this.btnTan, "btnTan");
-            this.btnTan.Name = "btnTan";
-            this.btnTan.UseVisualStyleBackColor = true;
-            this.btnTan.Click += new System.EventHandler(this.btnTan_Click);
-            // 
-            // btnCos
-            // 
-            resources.ApplyResources(this.btnCos, "btnCos");
-            this.btnCos.Name = "btnCos";
-            this.btnCos.UseVisualStyleBackColor = true;
-            this.btnCos.Click += new System.EventHandler(this.btnCos_Click);
-            // 
-            // btnSin
-            // 
-            resources.ApplyResources(this.btnSin, "btnSin");
-            this.btnSin.Name = "btnSin";
-            this.btnSin.UseVisualStyleBackColor = true;
-            this.btnSin.Click += new System.EventHandler(this.btnSin_Click);
-            // 
-            // btnLog1
-            // 
-            resources.ApplyResources(this.btnLog1, "btnLog1");
-            this.btnLog1.Name = "btnLog1";
-            this.btnLog1.UseVisualStyleBackColor = true;
-            this.btnLog1.Click += new System.EventHandler(this.btnLog1_Click);
-            // 
-            // btnLog
-            // 
-            resources.ApplyResources(this.btnLog, "btnLog");
-            this.btnLog.Name = "btnLog";
-            this.btnLog.UseVisualStyleBackColor = true;
-            this.btnLog.Click += new System.EventHandler(this.btnLog_Click);
-            // 
-            // btnSqrt
-            // 
-            resources.ApplyResources(this.btnSqrt, "btnSqrt");
-            this.btnSqrt.Name = "btnSqrt";
-            this.btnSqrt.UseVisualStyleBackColor = true;
-            this.btnSqrt.Click += new System.EventHandler(this.btnSqrt_Click);
-            // 
-            // btnPow
-            // 
-            resources.ApplyResources(this.btnPow, "btnPow");
-            this.btnPow.Name = "btnPow";
-            this.btnPow.UseVisualStyleBackColor = true;
-            this.btnPow.Click += new System.EventHandler(this.btnPow_Click);
-            // 
-            // btnSign
-            // 
-            resources.ApplyResources(this.btnSign, "btnSign");
-            this.btnSign.Name = "btnSign";
-            this.btnSign.UseVisualStyleBackColor = true;
-            this.btnSign.Click += new System.EventHandler(this.btnSign_Click);
-            // 
-            // btnAbs
-            // 
-            resources.ApplyResources(this.btnAbs, "btnAbs");
-            this.btnAbs.Name = "btnAbs";
-            this.btnAbs.UseVisualStyleBackColor = true;
-            this.btnAbs.Click += new System.EventHandler(this.btnAbs_Click);
-            // 
-            // btnMax
-            // 
-            resources.ApplyResources(this.btnMax, "btnMax");
-            this.btnMax.Name = "btnMax";
-            this.btnMax.UseVisualStyleBackColor = true;
-            this.btnMax.Click += new System.EventHandler(this.btnMax_Click);
-            // 
-            // btnMin
-            // 
-            resources.ApplyResources(this.btnMin, "btnMin");
-            this.btnMin.Name = "btnMin";
-            this.btnMin.UseVisualStyleBackColor = true;
-            this.btnMin.Click += new System.EventHandler(this.btnMin_Click);
-            // 
-            // btnClear
-            // 
-            resources.ApplyResources(this.btnClear, "btnClear");
-            this.btnClear.Name = "btnClear";
-            this.btnClear.UseVisualStyleBackColor = true;
-            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            resources.ApplyResources(this.txtID, "txtID");
+            this.txtID.Name = "txtID";
             // 
             // label1
             // 
             resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
             // 
+            // btn1
+            // 
+            resources.ApplyResources(this.btn1, "btn1");
+            this.btn1.Name = "btn1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
             // label2
             // 
             resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
+            // 
+            // txtYas
+            // 
+            resources.ApplyResources(this.txtYas, "txtYas");
+            this.txtYas.Name = "txtYas";
+            // 
+            // txtAd
+            // 
+            resources.ApplyResources(this.txtAd, "txtAd");
+            this.txtAd.Name = "txtAd";
             // 
             // label3
             // 
             resources.ApplyResources(this.label3, "label3");
             this.label3.Name = "label3";
             // 
-            // txtSecondNumber
+            // label4
             // 
-            resources.ApplyResources(this.txtSecondNumber, "txtSecondNumber");
-            this.txtSecondNumber.Name = "txtSecondNumber";
-            this.txtSecondNumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSecondNumber_KeyPress);
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
             // 
-            // txtFirstNumber
+            // txtPozisyon
             // 
-            resources.ApplyResources(this.txtFirstNumber, "txtFirstNumber");
-            this.txtFirstNumber.Name = "txtFirstNumber";
-            this.txtFirstNumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtFirstNumber_KeyPress);
-            // 
-            // txtResult
-            // 
-            resources.ApplyResources(this.txtResult, "txtResult");
-            this.txtResult.Name = "txtResult";
-            // 
-            // btnExit
-            // 
-            resources.ApplyResources(this.btnExit, "btnExit");
-            this.btnExit.Name = "btnExit";
-            this.btnExit.UseVisualStyleBackColor = true;
-            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            resources.ApplyResources(this.txtPozisyon, "txtPozisyon");
+            this.txtPozisyon.Name = "txtPozisyon";
             // 
             // mainform
             // 
             resources.ApplyResources(this, "$this");
-            this.Controls.Add(this.btnExit);
-            this.Controls.Add(this.txtResult);
-            this.Controls.Add(this.txtFirstNumber);
-            this.Controls.Add(this.txtSecondNumber);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPozisyon);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.txtYas);
+            this.Controls.Add(this.btn1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtID);
             this.Name = "mainform";
-            this.Load += new System.EventHandler(this.mainform_Load);
-            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -232,28 +107,15 @@
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button btnCopyFirst;
-        private System.Windows.Forms.Button btnExp;
-        private System.Windows.Forms.Button btnTan;
-        private System.Windows.Forms.Button btnCos;
-        private System.Windows.Forms.Button btnSin;
-        private System.Windows.Forms.Button btnLog1;
-        private System.Windows.Forms.Button btnLog;
-        private System.Windows.Forms.Button btnSqrt;
-        private System.Windows.Forms.Button btnPow;
-        private System.Windows.Forms.Button btnAbs;
-        private System.Windows.Forms.Button btnMax;
-        private System.Windows.Forms.Button btnMin;
-        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtID;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtYas;
+        private System.Windows.Forms.TextBox txtAd;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox txtSecondNumber;
-        private System.Windows.Forms.TextBox txtFirstNumber;
-        private System.Windows.Forms.TextBox txtResult;
-        private System.Windows.Forms.Button btnSign;
-        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPozisyon;
     }
 }
 
---
+++ ilk winform/Form2.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace ilk_winform
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.label4.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label4.Location = new System.Drawing.Point(30, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(241, 29);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Personel Pozisyon";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.label3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label3.Location = new System.Drawing.Point(30, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(169, 29);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Personel Adı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.label2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label2.Location = new System.Drawing.Point(30, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 29);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Personel Yaşı";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.label1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label1.Location = new System.Drawing.Point(30, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 29);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Personel ID";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}

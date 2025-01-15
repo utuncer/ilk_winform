3109d4a4e08cffb251163b6179dc68a8a2d8355c
Umut Tunçer <utuncerx@gmail.com>
2025-01-15 21:34:22 +0300

    System I/O (File and FileInfo)
    File.Exists, FileStream, StreamReader, StreamWriter
    , FileInfo, File, EndOfStream

--- ilk winform/Form1.Designer.cs
+++ ilk winform/Form1.Designer.cs
@@ -29,49 +29,78 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainform));
-            this.btn1 = new System.Windows.Forms.Button();
-            this.btn2 = new System.Windows.Forms.Button();
-            this.btn3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // btn1
+            // button1
             // 
-            resources.ApplyResources(this.btn1, "btn1");
-            this.btn1.Name = "btn1";
-            this.btn1.UseVisualStyleBackColor = true;
-            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // btn2
+            // button2
             // 
-            resources.ApplyResources(this.btn2, "btn2");
-            this.btn2.Name = "btn2";
-            this.btn2.UseVisualStyleBackColor = true;
-            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // btn3
+            // button3
             // 
-            resources.ApplyResources(this.btn3, "btn3");
-            this.btn3.Name = "btn3";
-            this.btn3.UseVisualStyleBackColor = true;
-            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // button4
+            // 
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            resources.ApplyResources(this.button5, "button5");
+            this.button5.Name = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // mainform
             // 
             resources.ApplyResources(this, "$this");
-            this.Controls.Add(this.btn3);
-            this.Controls.Add(this.btn2);
-            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "mainform";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
 
         #endregion
 
-        private System.Windows.Forms.Button btn1;
-        private System.Windows.Forms.Button btn2;
-        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- ilk winform/Form1.cs
+++ ilk winform/Form1.cs
@@ -19,42 +19,72 @@ namespace ilk_winform
         /*
         System I/O
         Dosya oluşturma, silme, yazma, kapatma gibi işlemleri yapmamızı sağlayan bir namespace dir
-        bu işlemler için kullanılan somut olmayan sınıflar -> https://prnt.sc/j53gN4mBL4GN
-        Directory ve Directory Sınıfları -> https://prnt.sc/_g8lCQ327koD / https://hizliresim.com/smxh08e / https://prnt.sc/Z_zGq4MW8Zhi
+        // kütüphanelerin arasna using System.IO; eklenmeli
+        File & FileInfo
+        File Metotları -> https://hizliresim.com/6cz4qkr
+        FileInfo -> https://hizliresim.com/gm4fxlt
+        FileInfo Metotları -> https://prnt.sc/FAXtgPtpqhZJ
+
+        Belirli bir kaynağı okumak, yazmak için Stream sınıfını kullanmak gerekli
+        File/Memory/Buffered/Netork/Pipe/Crypto Stream
         */
         public mainform()
         {
             InitializeComponent();
         }
 
-        private void btn1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-            //  Directory.CreateDirectory("temp"); //bin\Debug kısmına temp adında klasör oluşturacak
-            //DirectoryInfo dir = new DirectoryInfo("temp");
-            //dir.Create(); 
-            //
-            //dir.LastAccessTime.ToString();// -> dosyaya son erişim zamanı, CreationTime -> oluşturma zamanı
-
-            if (Directory.Exists("temp"))//bu dosya bin/debug'da var mı?
+            //  File.Create("Firstfile.txt");
+            if (!File.Exists("Firstfile.txt"))
             {
-                MessageBox.Show("Bu dosya zaten var!");
+                FileInfo outfile = new FileInfo("Firstfile.txt");
+                outfile.Create();
             }
             else
             {
-                Directory.CreateDirectory("temp");
-                MessageBox.Show("Dosya oluşturuldu...");
+                FileInfo outfile = new FileInfo("Firstfile.txt");
+                outfile.Delete();
+                outfile.Create();
             }
+
         }
 
-        private void btn2_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
-            // Directory.Move("yerini değiştirmek istediğin klasör","değiştirmek istediğin klasörün gideceği yol");
-            Directory.Move("temp", @"c:\temp");
+            //Filestreamin 3 parametresi işlem yapmak istediği dosyanın ismi/dosyanın olup olmaması yani kontrol mekanizması(FileMode.OpenOrCreate varsa aç yoksa oluştur)/ne işlem yapacaksın okuma ya da yazma
+            FileStream fs = new FileStream("third.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs); // tanımlanan filestreami burada parametrede gösteriyorsun
+            sw.WriteLine("Bu ilk satır");
+            sw.WriteLine("Bu ikinci satır");
+            sw.WriteLine("Bu üçüncü satır");
+            sw.Flush(); //metinleri dosayaya iletmek için kullanılır
+            sw.Close();
+            fs.Close(); //değiştirilen dsoyanın son haline görmek için her ikisinide kapatıyoruz
         }
 
-        private void btn3_Click(object sender, EventArgs e)
+        private void button3_Click(object sender, EventArgs e)
         {
-            Directory.Delete(@"c:\temp");
+            FileStream fs = new FileStream("third.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            while (!sr.EndOfStream) // sr.Close() yazılana kadar döngü devam edecek
+            {
+                textBox1.Text += sr.ReadLine() + Environment.NewLine;
+            }
+            sr.Close();
+            fs.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {//file copy parametreleri -> kaynak, hedef
+            File.Copy("secondfile.txt",@"C:\temp\secondfile.txt");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            FileInfo file = new FileInfo("third.txt");
+            MessageBox.Show(file.Name); //dosya adı
+            MessageBox.Show(file.FullName); //dosyanın adı yoluyla birlikte gelir
         }
     }
 }

b6c0be53e419e0b3e5b478807ca5ca92811e1640
Umut Tunçer <utuncerx@gmail.com>
2025-01-15 21:52:10 +0300

    System I/O (Path Class)
            GetDirectoryName
            GetExtension
            GetFileName
            GetFileNamWitoutExtension
            GetFullPath
            GetCurrentApplicationPath

--- ilk winform/Form1.Designer.cs
+++ ilk winform/Form1.Designer.cs
@@ -29,13 +29,14 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainform));
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
             // 
             // button1
             // 
@@ -44,48 +45,11 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
-            // 
-            resources.ApplyResources(this.button2, "button2");
-            this.button2.Name = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            resources.ApplyResources(this.button3, "button3");
-            this.button3.Name = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // textBox1
-            // 
-            resources.ApplyResources(this.textBox1, "textBox1");
-            this.textBox1.Name = "textBox1";
-            // 
-            // button4
-            // 
-            resources.ApplyResources(this.button4, "button4");
-            this.button4.Name = "button4";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // button5
-            // 
-            resources.ApplyResources(this.button5, "button5");
-            this.button5.Name = "button5";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
-            // 
             // mainform
             // 
             resources.ApplyResources(this, "$this");
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
             this.Name = "mainform";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -95,12 +59,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- ilk winform/Form1.cs
+++ ilk winform/Form1.cs
@@ -20,71 +20,31 @@ namespace ilk_winform
         System I/O
         Dosya oluşturma, silme, yazma, kapatma gibi işlemleri yapmamızı sağlayan bir namespace dir
         // kütüphanelerin arasna using System.IO; eklenmeli
-        File & FileInfo
-        File Metotları -> https://hizliresim.com/6cz4qkr
-        FileInfo -> https://hizliresim.com/gm4fxlt
-        FileInfo Metotları -> https://prnt.sc/FAXtgPtpqhZJ
-
-        Belirli bir kaynağı okumak, yazmak için Stream sınıfını kullanmak gerekli
-        File/Memory/Buffered/Netork/Pipe/Crypto Stream
+        Dosya isimleri/uzantılarına ulaşmak için path class kullanılır
+        GetDirectoryName
+        GetExtension
+        GetFileName
+        GetFileNamWitoutExtension
+        GetFullPath
+        GetCurrentApplicationPath
         */
         public mainform()
         {
             InitializeComponent();
         }
 
+        #region
+
+        #endregion
+
         private void button1_Click(object sender, EventArgs e)
         {
-            //  File.Create("Firstfile.txt");
-            if (!File.Exists("Firstfile.txt"))
-            {
-                FileInfo outfile = new FileInfo("Firstfile.txt");
-                outfile.Create();
-            }
-            else
-            {
-                FileInfo outfile = new FileInfo("Firstfile.txt");
-                outfile.Delete();
-                outfile.Create();
-            }
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            //Filestreamin 3 parametresi işlem yapmak istediği dosyanın ismi/dosyanın olup olmaması yani kontrol mekanizması(FileMode.OpenOrCreate varsa aç yoksa oluştur)/ne işlem yapacaksın okuma ya da yazma
-            FileStream fs = new FileStream("third.txt", FileMode.OpenOrCreate, FileAccess.Write);
-            StreamWriter sw = new StreamWriter(fs); // tanımlanan filestreami burada parametrede gösteriyorsun
-            sw.WriteLine("Bu ilk satır");
-            sw.WriteLine("Bu ikinci satır");
-            sw.WriteLine("Bu üçüncü satır");
-            sw.Flush(); //metinleri dosayaya iletmek için kullanılır
-            sw.Close();
-            fs.Close(); //değiştirilen dsoyanın son haline görmek için her ikisinide kapatıyoruz
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            FileStream fs = new FileStream("third.txt", FileMode.Open, FileAccess.Read);
-            StreamReader sr = new StreamReader(fs);
-            while (!sr.EndOfStream) // sr.Close() yazılana kadar döngü devam edecek
-            {
-                textBox1.Text += sr.ReadLine() + Environment.NewLine;
-            }
-            sr.Close();
-            fs.Close();
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {//file copy parametreleri -> kaynak, hedef
-            File.Copy("secondfile.txt",@"C:\temp\secondfile.txt");
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-            FileInfo file = new FileInfo("third.txt");
-            MessageBox.Show(file.Name); //dosya adı
-            MessageBox.Show(file.FullName); //dosyanın adı yoluyla birlikte gelir
+            textBox1.Text += Path.GetDirectoryName(@"C:\new folder\temp\forest.png")+Environment.NewLine; //dosyanın olduğu klasörü yazar C:\new folder\temp\
+            textBox1.Text += Path.GetFileName(@"C:\new folder\temp\forest.png")+Environment.NewLine;
+            textBox1.Text += Path.GetFileNameWithoutExtension(@"C:\new folder\temp\forest.png")+Environment.NewLine; 
+            textBox1.Text += Path.GetExtension(@"C:\new folder\temp\forest.png")+Environment.NewLine; 
+            textBox1.Text += Path.GetPathRoot(@"C:\new folder\temp\forest.png")+Environment.NewLine; 
+            textBox1.Text += Path.GetFullPath(@"C:\new folder\temp\forest.png")+Environment.NewLine; 
         }
     }
 }
